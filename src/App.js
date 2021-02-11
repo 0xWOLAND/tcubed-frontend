83d5143f08bf6bd431427945d0cef572a119cd6c
bhargavannem <bhargav.annem@gmail.com>
2021-02-11 08:50:21 -0600

    initial files

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div
+        class="btn-group mt-2 mb-4"
+        role="group"
+        aria-label="actionButtons"
+      ></div>
+
+      <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js"
+        integrity="sha256-qM7QTJSlvtPSxVRjVWNM2OfTAz/3k5ovHOKmKXuYMO4="
+        crossorigin="anonymous"
+      ></script>
+    </div>
+  );
+}
+
+export default App;
