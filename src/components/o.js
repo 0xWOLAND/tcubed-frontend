a17fdca0ec8923f39a3767436f0c4bb9d117c329
bhargavannem <bhargav.annem@gmail.com>
2021-02-11 18:53:51 -0600

    svg animations

---
+++ src/components/o.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./style.css";
+function o() {
+  return (
+    <div>
+      <svg
+        width="71"
+        height="71"
+        viewBox="0 0 71 71"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className="path"
+      >
+        <circle
+          cx="35.5"
+          cy="35.5"
+          r="30.5"
+          fill="white"
+          stroke="black"
+          stroke-width="10"
+        />
+      </svg>
+    </div>
+  );
+}
+
+export default o;
---
+++ src/components/x.js
@@ -0,0 +1,108 @@
+import React from "react";
+import "./style.css";
+function x() {
+  return (
+    <div>
+      <svg
+        width="68"
+        height="68"
+        viewBox="0 0 68 68"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className="path"
+      >
+        <g filter="url(#filter0_d)">
+          <line
+            x1="7.53553"
+            y1="4.46447"
+            x2="59.5355"
+            y2="56.4645"
+            stroke="black"
+            stroke-width="10"
+          />
+        </g>
+        <defs>
+          <filter
+            id="filter0_d"
+            x="0"
+            y="0.928932"
+            width="67.0711"
+            height="67.0711"
+            filterUnits="userSpaceOnUse"
+            color-interpolation-filters="sRGB"
+          >
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feColorMatrix
+              in="SourceAlpha"
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            />
+            <feOffset dy="4" />
+            <feGaussianBlur stdDeviation="2" />
+            <feColorMatrix
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+            />
+            <feBlend
+              mode="normal"
+              in2="BackgroundImageFix"
+              result="effect1_dropShadow"
+            />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="effect1_dropShadow"
+              result="shape"
+            />
+          </filter>
+        </defs>
+        <g filter="url(#filter0_d)">
+          <line
+            x1="8.46447"
+            y1="56.4645"
+            x2="60.4645"
+            y2="4.46446"
+            stroke="black"
+            stroke-width="10"
+          />
+        </g>
+        <defs>
+          <filter
+            id="filter0_d"
+            x="0.928955"
+            y="0.928932"
+            width="67.0711"
+            height="67.0711"
+            filterUnits="userSpaceOnUse"
+            color-interpolation-filters="sRGB"
+          >
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feColorMatrix
+              in="SourceAlpha"
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            />
+            <feOffset dy="4" />
+            <feGaussianBlur stdDeviation="2" />
+            <feColorMatrix
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+            />
+            <feBlend
+              mode="normal"
+              in2="BackgroundImageFix"
+              result="effect1_dropShadow"
+            />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="effect1_dropShadow"
+              result="shape"
+            />
+          </filter>
+        </defs>
+      </svg>
+    </div>
+  );
+}
+export default x;
