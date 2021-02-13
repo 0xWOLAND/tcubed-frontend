d44796250d3b106210595736f1249dd08e0d3069
bhargavannem <bhargav.annem@gmail.com>
2021-02-13 10:31:32 -0600

    test

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
 import X from "./x";
-import O from "./o"
+import O from "./o";
+
 var count = 0;
 const x = X;
-const o = O
+const o = O;
+
+var boxes = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
 function Board() {
   return (
     <div id="board_container">
@@ -37,12 +41,30 @@ function Board() {
     </div>
   );
 }
+
+function resetBoard() {
+  for (let i = 0; i < 9; i++) {
+    boxes[i] += 1;
+    console.log(boxes[i]);
+  }
+}
+
 function Box(props) {
   const [select, setSelect] = useState();
+
+  useEffect(() => {
+    setSelect(null);
+  }, []);
+
   const handleClick = (id) => {
     let boxElement = document.getElementById(id);
     console.log(boxElement);
     let boxClass = boxElement.className;
+    if (count == 9) {
+      resetBoard();
+      console.log("ye")
+      count = 0;
+    }
     if (
       boxClass[boxClass.length - 1] != "X" &&
       boxClass[boxClass.length - 1] != "O" &&
