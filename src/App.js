dfd1587c437a18530c626d5dc0cb33b13c5880ba
bhargavannem <bhargav.annem@gmail.com>
2021-02-14 15:03:19 -0600

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Board from "./components/BoardClass";
+import React from "react";
+
 function App() {
   return (
     <div id="container">
